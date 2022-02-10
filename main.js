a21ee244b6b0316a91ef6b74d3214b92ff0093a4
itsmonayem <itsmonayem@gmail.com>
2022-02-10 10:55:46 +0600

    first phase

---
+++ main.js
@@ -0,0 +1,12 @@
+function toggle(){
+    let tog = document.getElementById('project1');
+    tog.classList.toggle('active1');
+    let contentToggle = document.getElementById('content');
+    contentToggle.classList.toggle('active');
+}
+function toggle2(){
+    let tog2 = document.getElementById('project2');
+    tog2.classList.toggle('active2');
+    let contentToggle = document.getElementById('content');
+    contentToggle.classList.toggle('active');
+}
